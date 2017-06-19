47b50fc122540b21767413844f4d88fe7ae3ce36
amit Inbar <amitinbar@h-macbook-pro-sl-amit.local>
2017-06-19 21:09:46 +0300

    Lecture 8 - Add firebase auth to your project

--- LearnFirebase/ViewController.swift
+++ LearnFirebase/ViewController.swift
@@ -7,14 +7,33 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
+    var handle: AuthStateDidChangeListenerHandle?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        handle = Auth.auth().addStateDidChangeListener({ (auth, user) in
+            // ...
+            if user != nil {
+                print("Signed in")
+            } else if user == nil {
+                print("Not signed in")
+            } else {
+                print("there is an error")
+            }
+        })
+    }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        Auth.auth().removeStateDidChangeListener(handle!)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
